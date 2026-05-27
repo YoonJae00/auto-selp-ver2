7b81f9206ae7540bbc80479bc144de8a33b7af5a
yoonjae <realyoon77@gmail.com>
2026-05-27 14:10:11 +0900

    test(processor): isolate database settings setup

--- services/processor/tests/test_database.py
+++ services/processor/tests/test_database.py
@@ -1,3 +1,18 @@
+import os
+
+os.environ.setdefault("DATABASE_URL", "postgresql+asyncpg://test:test@localhost/test")
+os.environ.setdefault("NAVER_API_KEY", "test")
+os.environ.setdefault("NAVER_SECRET_KEY", "test")
+os.environ.setdefault("NAVER_CUSTOMER_ID", "test")
+os.environ.setdefault("NAVER_CLIENT_ID", "test")
+os.environ.setdefault("NAVER_CLIENT_SECRET", "test")
+os.environ.setdefault("Coupang_Access_Key", "test")
+os.environ.setdefault("Coupang_Secret_Key", "test")
+os.environ.setdefault("GEMINI_API_KEY", "test")
+os.environ.setdefault("OPENAI_API_KEY", "test")
+os.environ.setdefault("KIPRIS_API_KEY", "test")
+os.environ.setdefault("INTERNAL_SERVICE_TOKEN", "internal-test-token")
+
 from database import SessionLocal
 
 
